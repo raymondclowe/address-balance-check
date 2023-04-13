997bcbe3b80b92d7cf4a0f098af15608a2098cae
Raymond Lowe <raymondclowe@gmail.com>
2023-04-13 18:39:21 +0800

    works but slow

--- address-balance-check.py
+++ address-balance-check.py
@@ -1,7 +1,19 @@
-import csv
+
 import os
+import psutil
 import subprocess
 import json
+import csv
+import sys
+
+wallets_folder ="./wallets"
+
+def is_electrum_running():
+    for proc in psutil.process_iter(['name']):
+        if 'electrum' in proc.info['name'].lower() and proc.info['name'].endswith('.exe'):
+            return True
+    return False
+
 
 
 def find_electrum_path():
@@ -28,65 +40,75 @@ def find_electrum_path():
 
 
 
-def start_daemon(electrum_path):
-    print("start: start_daemon")
-    # start Electrum daemon as a subprocess
-    daemon_proc = subprocess.Popen([electrum_path, "daemon"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # wait for the daemon to start
-    output, error = daemon_proc.communicate()
-    if daemon_proc.returncode != 0:
-        print(f"Error starting daemon: {error}")
-        exit()
-    print("end: start_daemon")
+# check if this is main so it can be imported into a module
+if __name__ == "__main__":
+    print("This is the main module.")
 
+    electrum_path = find_electrum_path()
 
+    # if electrum is not running then
+    if not is_electrum_running():
+        # spawn electrum as a subprocess and don't wait, pass the parameter "daemon" and get the pid        
+        # spawn Electrum as a subprocess and pass the "daemon" parameter
+        electrum_process = subprocess.Popen([electrum_path, "daemon"])
 
-def stop_daemon(electrum_path):
-    print("start: stop_daemon")
-    # stop the Electrum daemon
-    proc = subprocess.Popen([electrum_path, "stop"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = proc.communicate()
-    if proc.returncode != 0:
-        print(f"Error stopping daemon: {error}")
-    print("end: stop_daemon")
-
-
-
-def get_wallet_files(wallets_folder):
-    print("start: get_wallet_files")
-    # find all the wallet files in the folder
-    wallet_files = [os.path.join(wallets_folder, f) for f in os.listdir(wallets_folder) if f.endswith(".dat")]
-    return wallet_files
-    print("end: get_wallet_files")
-
-
-def get_wallet_data(wallet_file, electrum_path):
-    print("start: get_wallet_data")
+        # get the process ID of Electrum
+        electrum_pid = electrum_process.pid
+   
+    wallet_files = [os.path.join(wallets_folder, f) for f in os.listdir(wallets_folder) if os.path.isfile(os.path.join(wallets_folder, f))]
+    
     data_list = []
-    # open the wallet using Electrum CLI
-    proc = subprocess.Popen([electrum_path, "-w", wallet_file, "listaddresses"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = proc.communicate()
-    if proc.returncode != 0:
-        print(f"Error opening wallet {wallet_file}: {error}")
-        return []
-    
-    # parse the output and get the list of addresses
-    addresses = json.loads(output.decode())
-    
-    # for each address, get the balance and transaction count
-    for address in addresses:
-        # get the balance
-        proc = subprocess.Popen([electrum_path, "-w", wallet_file, "getaddressbalance", address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for wallet_file in wallet_files:
+        
+        # open the wallet using Electrum CLI
+
+        proc = subprocess.Popen([electrum_path, "-w", wallet_file, "load_wallet"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = proc.communicate()
         if proc.returncode != 0:
-            print(f"Error getting balance for address {address}: {error}")
-            return []
+            print(f"Error opening wallet {wallet_file}: {error}")
+            break
 
-        # parse the output and get the balance and transaction count
-        balance_data = json.loads(output.decode())
-        balance = balance_data["confirmed"]
-        tx_count = balance_data["history"]
+        # open the wallet using Electrum CLI
+        
+        proc = subprocess.Popen([electrum_path, "-w", wallet_file, "listaddresses"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = proc.communicate()
+        if proc.returncode != 0:
+            print(f"Error listing addresses in wallet {wallet_file}: {error}")
+            break
 
-        # add the data to the list
-        data_list
+        # parse the output and get the list of addresses
+        addresses = json.loads(output.decode())
+        
+        # for each address, get the balance and transaction count
+        for address in addresses:
+            # get the balance
+            proc = subprocess.Popen([electrum_path, "-w", wallet_file, "getaddressbalance", address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = proc.communicate()
+            if proc.returncode != 0:
+                print(f"Error getting balance for address {address}: {error}")
+                break
+
+            # parse the output and get the balance and transaction count
+            balance_data = json.loads(output.decode())
+            balance = balance_data["confirmed"]
+
+            # get the history
+            proc = subprocess.Popen([electrum_path, "-w", wallet_file, "getaddresshistory", address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = proc.communicate()
+            if proc.returncode != 0:
+                print(f"Error getting history for address {address}: {error}")
+                break
+
+            # parse the output and get the balance and transaction count
+            history_data = json.loads(output.decode())
+
+            tx_count = len(history_data)
+
+            # add the data to the list
+            wallet_filename = wallet_file.split('\\')[-1] 
+            data_list.append([wallet_filename, address, balance, tx_count])
+            print(f'{wallet_filename},{address},{balance},{tx_count}')
+
+# else:
+#     print("This module has been imported.")
